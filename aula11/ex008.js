441b62c9003e8a401ea5243e478fc77a118e753d
marcos373003 <marcos373003@gmail.com>
2025-11-11 11:13:23 -0300

    atualizei

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 100 
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if(vel > 60){ //Condição simples!
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando o cinto de segurança`)
+
